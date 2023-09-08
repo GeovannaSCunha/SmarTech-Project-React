d43d6bc5248266ee9433091f64eeee3ff78794ae
GeovannaSCunha <rm97736@fiap.com.br>
2023-09-08 18:45:05 -0300

    Tentativa de add rotas e criando o conteudo da pagina Quem somos

---
+++ src/routes/QuemSomos.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Cabecalho from '../components/Cabecalho';
+import Rodape from '../components/Rodape';
+import Conteudo from '../components/PagQuemSomos'
+const quemSomos = () => (
+    <>
+    <Cabecalho/>
+    <Conteudo/>
+    <Rodape/>
+    </>
+);
+
+export default quemSomos;
