5b077f89545634fc4fc7af4e611787f3d736146a
fernandakaory <rm551104@fiap.com.br>
2023-09-09 21:30:11 -0300

    arrumando texto conteudosus pt2

--- src/components/ConteudoSus.jsx
+++ src/components/ConteudoSus.jsx
@@ -8,7 +8,7 @@ export default function sus() {
             <h1>O que é a Sustentabilidade?</h1>
 
             <section>
-                <p>Com a sustentabilidade, podemos suprir as necessidades atuais sem prejudicar o meio ambiente e as futuras gerações. A partir do conceito de desenvolvimento sustentável, pesquisamos formas de aprimorar as técnicas atuais para utilizar dos recursos disponíveis, tendo em consideração a preservação do planeta.</p>
+                <p>Com a sustentabilidade, podemos suprir as necessidades atuais sem prejudicar o meio ambiente e as futuras gerações. A partir do conceito de desenvolvimento sustentável, pesquisamos formas de aprimorar as técnicas atuais para utilizar dos recursos disponíveis, considerando a preservação do planeta.</p>
 
                 <img src={img6} alt="Planeta Terra sustentavel" width="80%"/>
             </section>
