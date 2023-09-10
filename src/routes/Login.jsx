cfed314b255d60d31467fdf86238ec76d7b43080
MirellaRossi <rm97990@fiap.com.br>
2023-09-10 14:11:16 -0300

    Alterações no formulário

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
-import { StyledMain } from '../components/Styles'
+import {StyledLogin} from '../components/Styles'
 
 export default function formsone() {
   return (
-    <StyledMain>
-      <h1>Acesse a plataforma</h1>
-      <form>
-        <label for="c-user">Nome de Usuário</label>
-        <input type="text" name="user" id="c-user"></input>
-        <label for="c-senha">Senha</label>
-        <input type="password" name="senha" id="c-senha"></input>
-
-        <button type='submit'>Login</button>
-      </form>
-    </StyledMain>
+    <StyledLogin>
+    <form class="form">
+    Acesse a plataforma
+    <input type="text" class="input" placeholder="Usuário"></input>
+    <input type="text" class="input" placeholder="Senha"></input> 
+    <button>Login</button>
+    </form>
+    </StyledLogin>
 
   )
 }
