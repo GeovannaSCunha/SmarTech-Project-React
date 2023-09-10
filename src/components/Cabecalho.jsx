8caea8c1712b246ba8643fe0337ab2b51934a39c
fernandakaory <rm551104@fiap.com.br>
2023-09-09 21:24:05 -0300

    arrumando textos cabecalho

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -12,13 +12,13 @@ export default function Cabecalho() {
                     <Link id="logo" to="/"><img src={Logo} alt="SmarTech" width="240px"/></Link>
                     
                         <ul>
-                            <li><Link to="/sobre-nos">Quem Somos</Link> </li>
-                            <li><Link to="/sustentabilidade">Sustentabilidade</Link> </li>
-                            <li><Link to="/tecnologia">Tecnologia</Link> </li>
-                            <li><Link to="/problema">Problema</Link> </li>
-                            <li><Link to="/solucoes">Soluções</Link> </li>
-                            <li><Link to="/clientes">Nossos Clientes</Link> </li>
-                            <li><Link to="/">Login</Link> </li>
+                            <li><Link to="/sobre-nos">Quem  Somos</Link> </li>
+                            <li><Link to="/sustentabilidade">Sustentabilidade </Link> </li>
+                            <li><Link to="/tecnologia">Tecnologia </Link> </li>
+                            <li><Link to="/problema">Problema </Link> </li>
+                            <li><Link to="/solucoes">Soluções </Link> </li>
+                            <li><Link to="/clientes">Nossos Clientes </Link> </li>
+                            <li><Link to="/">Login </Link> </li>
                         </ul>
                 </div>
             </nav>
