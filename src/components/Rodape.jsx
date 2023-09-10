92850c084b6441a8fbb977270c09846f65a87534
fernandakaory <rm551104@fiap.com.br>
2023-09-09 21:31:58 -0300

    adicionando mais informação ao rodape

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -9,7 +9,7 @@ export default function Rodape() {
             <img src={Logo2} alt="" width="50px"/>
             <p>Av. Paulista, 1106 - 4º andar</p>
             <p>Bela Vista, São Paulo - SP, 01311-000</p>
-            <p>Sprint - Engenharia de Software</p>
+            <p>Sprint 3 - Engenharia de Software</p>
             <p><b>FIAP</b></p>
         </footer>
     </>
