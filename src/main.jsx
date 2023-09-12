8f409709654e51831d593844c228e63260a09200
LanaAuada <rm551143@fiap.com.br>
2023-09-11 21:48:45 -0300

    add paths da main

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Erro404 from './routes/Error404.jsx'
 
+
+// 1- Configurando o Router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import NC from './routes/NossosClientes.jsx';
+import Problema from './routes/Problema.jsx';
+import QS from './routes/QuemSomos.jsx';
+import Solucoes from './routes/Solucoes.jsx';
+import Sus from './routes/Sustentabilidade.jsx';
+import Tech from './routes/Tecnologia.jsx';
+import Home from './components/Home.jsx';
+import Login from './routes/Login.jsx';
+import Feedback from './routes/Feedback.jsx';
+
+const router = createBrowserRouter([
+  {path: "/", element: <App />, errorElement: <Erro404/>,
+  children: [
+    {path: "/", element: <Home/>},
+    {path: "/clientes", element: <NC/>},
+    {path: "/problema", element: <Problema/>},
+    {path: "/sobre-nos", element: <QS/>},
+    {path: "/solucoes", element: <Solucoes/>},
+    {path: "/sustentabilidade", element: <Sus/>},
+    {path: "/tecnologia", element: <Tech/>},
+    {path: "/login", element: <Login/>},
+    {path: "/feedback", element: <Feedback/>}
+
+  ]},
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+);
