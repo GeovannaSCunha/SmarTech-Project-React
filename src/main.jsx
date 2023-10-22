1536333e1e365f59061457aa746ac3a130ef5ca1
GeovannaSCunha <geovanna.scunha@gmail.com>
2023-10-21 22:13:28 -0300

    Aplicando o estilo Global com o Styled Components

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import GlobalStyle from './styles/EstiloGlobal.js'
 import Erro404 from './routes/Error404.jsx'
 
 
@@ -44,6 +44,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle/>
     <RouterProvider router={router}/>
   </React.StrictMode>,
 );
---
+++ src/styles/EstiloGlobal.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
+`
+export default GlobalStyle
