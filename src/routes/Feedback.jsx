816423b0b62453344aa1ed1cf491afee01b68f5b
GeovannaSCunha2 <geovanna.scunha@gmail.com>
2023-11-07 09:09:51 -0300

    Resolvendo erro com uma função

--- src/routes/Feedback.jsx
+++ src/routes/Feedback.jsx
@@ -1,67 +1,45 @@
 import '../assets/styles/Feedback.css'
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+ 
 export default function Avalia() {
   const [formData, setFormData] = useState({
     zona: "",
-    transporte: [],
+    transporte: "",
     linha: "",
     obs: "",
   });
-
+ 
   const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-
-    if (type === "checkbox") {
-      if (checked) {
-        setFormData({
-          ...formData,
-          transporte: [...formData.transporte, value],
-        });
-      } else {
-        setFormData({
-          ...formData,
-          transporte: formData.transporte.filter((item) => item !== value),
-        });
-      }
-    } else {
-      setFormData({
-        ...formData,
-        [name]: value,
-      });
-    }
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
-
+ 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
   };
-
+ 
   return (
-    
-      <form  className="formulario"onSubmit={handleSubmit}>
-        <h4>Qual a região em que você reside?</h4>
-       <input type="text" className= "input" placeholder="Central, Sul, Norte, Oeste"/>
-        
-        <h4>Qual o meio de transporte que você mais utiliza?</h4>
-        <input type="text" className="input" placeholder="Ônibus, Trem, Metrô, Bicicleta" />
-        
-        <h4>Qual a linha que você mais utiliza?</h4>
-        <input type="text" className="input" placeholder="Linha" />
-        
-    
-        <h5>Registre aqui suas sugestões, elogio e reclamações</h5>
-        <input type="text" className="input" placeholder="" />
-        
-        <Link to="/Principal">
-          <button  className="btn-enviar" type="submit">Enviar</button>
-        </Link>
-        
-        <Link to="/Principal">
-          <button id="v">Voltar</button>
-        </Link>
-      </form>
-  
+<form  className="formulario"onSubmit={handleSubmit}>
+<h4>Qual a região em que você reside?</h4>
+<input type="text" className= "input" placeholder="Central, Sul, Norte, Oeste" name="zona" onChange={handleChange}/>
+<h4>Qual o meio de transporte que você mais utiliza?</h4>
+<input type="text" className="input" placeholder="Ônibus, Trem, Metrô, Bicicleta" name="transporte" onChange={handleChange}/>
+<h4>Qual a linha que você mais utiliza?</h4>
+<input type="text" className="input" placeholder="Linha" name="linha" onChange={handleChange}/>
+
+<h5>Registre aqui suas sugestões, elogio e reclamações</h5>
+<input type="text" className="input" placeholder="" name="obs" onChange={handleChange}/>
+<Link to="/Principal">
+<button  className="btn-enviar" type="submit">Enviar</button>
+</Link>
+<Link to="/Principal">
+<button id="v">Voltar</button>
+</Link>
+</form>
   );
 }
